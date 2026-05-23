cf82a534d388435cfc0704cd6b3a93b0bc1f89d7
tidingdbruh <tidingdbruh@gmail.com>
2026-05-23 22:09:44 +0530

    new file 03 and edited 02 file

--- 02-shell-variable.sh
+++ 02-shell-variable.sh
@@ -2,4 +2,7 @@
 
 Variable_Name='bash'  #No space before and after '='
 
+Name='bobby'
+
+echo " Hello, ${Name}. Welcome!"
 echo "This script is written in $Variable_Name "  #Use $ while using the variable
---
+++ 03-shell-variable.sh
@@ -0,0 +1,5 @@
+#This is read variable value from user.
+
+read name
+
+echo "Hello, $name. Welcome!"
