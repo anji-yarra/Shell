59f3f87044fcd570a7812dd5a41fa48ea619c3a8
tidingdbruh <tidingdbruh@gmail.com>
2026-05-23 16:18:24 +0530

    commiting 01 and 02 files

---
+++ 01-Hello-world.sh
@@ -0,0 +1,5 @@
+#this is the first bash script file
+
+# ECHO is used to print the matter
+
+echo "Hello, World!"
---
+++ 02-shell-variable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+Variable_Name='bash'  #No space before and after '='
+
+echo "This script is written in $Variable_Name "  #Use $ while using the variable
