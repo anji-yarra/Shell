dd405f80d2aa704f1b2aa097b9636dcdf04bbd7d
tidingdbruh <tidingdbruh@gmail.com>
2026-05-26 03:47:03 +0530

    adding 04 and 05 files

---
+++ 05-special-variables.sh
@@ -0,0 +1,9 @@
+#!/bib/bash
+
+echo "The script name is $0"
+echo "The first argument is $1"
+echo "The second argument is $3"
+echo "Number of arguments are $#"
+echo "All arguments as a list $@"
+echo "Exit status of the last command $?"
+echo "Process ID of the current script $$"
