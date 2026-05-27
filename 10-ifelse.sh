7e49307b8dbecc5f52cb13248a5eff0ce3dae11f
tidingdbruh <tidingdbruh@gmail.com>
2026-05-27 12:49:58 +0530

    .

---
+++ 10-ifelse.sh
@@ -0,0 +1,14 @@
+read -p "Enter your marks" Marks
+
+if [ $Marks -ge 90 ]
+then
+    echo "Grade A"
+elif [ $Marks -ge 70 ]
+then 
+    echo "Grade B"
+elif [ $Marks -ge 50]
+then
+    echo "Grade C"
+else 
+    echo "Fail"
+fi
