ff9c2d9ed53b19a9379100c22602b99f2accb7dd
tidingdbruh <tidingdbruh@gmail.com>
2026-05-27 11:49:05 +0530

    .\08-data-types.sh

---
+++ 08-data-types.sh
@@ -0,0 +1,10 @@
+Name="Bobby" #String datatype
+echo "My name is $Name"
+
+Age=27 #Numeric Datatype
+
+echo "I'm $Age years old"
+
+
+Fruits=("Apple","Banana","Mango")
+echo "My favorite fruits are ${fruits[0]}, ${fruits[1]}, ${fruits[2]}"
