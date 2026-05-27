bcd6a55c9c6b81a62e7554297a1facce83e46ef8
tidingdbruh <tidingdbruh@gmail.com>
2026-05-27 11:26:59 +0530

    Modified special variable file

--- 05-special-variables.sh
+++ 05-special-variables.sh
@@ -1,8 +1,8 @@
-#!/bib/bash
+#!/bin/bash
 
 echo "The script name is $0"
 echo "The first argument is $1"
-echo "The second argument is $3"
+echo "The second argument is $2"
 echo "Number of arguments are $#"
 echo "All arguments as a list $@"
 echo "Exit status of the last command $?"
